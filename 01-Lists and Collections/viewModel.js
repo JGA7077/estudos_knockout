5a8ace3499dfc6597f64d84179a63d85c427d2c9
João Araújo <joaogabriel-al@hotmail.com>
2022-08-26 07:35:51 -0300

    trocando JS para o da DOC

--- 01-Lists and Collections/viewModel.js
+++ 01-Lists and Collections/viewModel.js
@@ -1,31 +1,26 @@
-// Classe para representar uma linha na grade de reservas de assento
-class SeatReservation {
-  constructor(name, initialMeal) {
-    let self = this
-    self.name = name
-    self.meal = ko.observable(initialMeal)
-  }
+// Class to represent a row in the seat reservations grid
+function SeatReservation(name, initialMeal) {
+  var self = this;
+  self.name = name;
+  self.meal = ko.observable(initialMeal);
 }
 
-// ViewModel geral para esta tela, juntamente com o estado inicial
-class ReservationsViewModel {
-  constructor() {
-    let self = this
+// Overall viewmodel for this screen, along with initial state
+function ReservationsViewModel() {
+  var self = this;
 
-    // Dados de catalogo não editáveis - viria de um servidor
-    self.availablesMeals = [
-      {mealName: "Standard (sandwich)", price: 0},
-      {mealName: "Premium (lobster)", price: 34.95},
-      {mealName: "Ultimate (whole zebra)", price: 290}
-    ]
+  // Non-editable catalog data - would come from the server
+  self.availableMeals = [
+      { mealName: "Standard (sandwich)", price: 0 },
+      { mealName: "Premium (lobster)", price: 34.95 },
+      { mealName: "Ultimate (whole zebra)", price: 290 }
+  ];    
 
-    // Dados editáveis
-    self.seats = ko.observableArray([
-      new SeatReservation("João", self.availablesMeals[0]),
-      new SeatReservation("Bert", self.availablesMeals[1]),
-      new SeatReservation("Steve", self.availablesMeals[2])
-    ])
-  }
+  // Editable data
+  self.seats = ko.observableArray([
+      new SeatReservation("Steve", self.availableMeals[0]),
+      new SeatReservation("Bert", self.availableMeals[0])
+  ]);
 }
 
-ko.applyBindings(new ReservationsViewModel())
+ko.applyBindings(new ReservationsViewModel());
